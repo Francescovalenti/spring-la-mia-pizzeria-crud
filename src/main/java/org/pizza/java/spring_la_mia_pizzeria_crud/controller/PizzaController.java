bc07db7493f45c8b3f91bca1d9572f3d64e0fd17
francescovalenti <francescovalenti83@outlook.it>
2025-06-19 17:52:09 +0200

    feat:inserito show e bonus

--- src/main/java/org/pizza/java/spring_la_mia_pizzeria_crud/controller/PizzaController.java
+++ src/main/java/org/pizza/java/spring_la_mia_pizzeria_crud/controller/PizzaController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -17,11 +19,27 @@ public class PizzaController {
     @Autowired
     private PizzaRepository pizzaRepository;
 
-   @GetMapping
-    public String index(Model model) {
-       
-        List<Pizza> pizzas = pizzaRepository.findAll();
+  
+    @GetMapping("/{id}") //
+    public String show(@PathVariable("id") Integer id, Model model) {
+        model.addAttribute("pizza", pizzaRepository.findById(id).get());
+        return "pizzas/show";
+    }
+
+    @GetMapping
+    public String index(@RequestParam(name = "keyword", required = false) String keyword, Model model) {
+        List<Pizza> pizzas;
+
+        if (keyword != null && !keyword.isEmpty()) {
+            pizzas = pizzaRepository.findByNameContainingIgnoreCase(keyword);
+        } else {
+            pizzas = pizzaRepository.findAll();
+        }
+
         model.addAttribute("pizzas", pizzas);
+        model.addAttribute("keyword", keyword);
+
         return "pizzas/index";
     }
+
 }
